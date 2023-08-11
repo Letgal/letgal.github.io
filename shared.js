921e2f9bb1493dcbe92040f22354abcd0de20060
Letgal <leton.ostislavic@gmail.com>
2023-08-11 13:15:58 +0300

    Оновлення (+ світла ѳема)
    Додано світлу ѳему (бета-версія) й зроблено кілька дрібних змін

--- shared.js
+++ shared.js
@@ -1,18 +1,52 @@
 const headerButtons = [
-	{ text: "<img>", link: "/" },
-	{ text: "Переклади ігор Paradox", link: "/ut.html" },
-	// { text: "Переклади модів Skyrim", link: "/ut-skyrim.html" },
-	// { text: "Транскрибування", link: "/transcript.html" },
-	// { text: "Новини Hearts of Iron IV", link: "/hoi4-news" },
-	{ text: "Підтримати аўтора", link: "https://send.monobank.ua/jar/2hW6s9nEeY" }
+	{
+		tag: "a",
+		text: "<img>",
+		attr: "href", value: "/"
+	},
+	{
+		tag: "a",
+		text: "Укр. переклади ігор Paradox",
+		attr: "href", value: "/ut.html"
+	},
+	// {
+	// 	tag: "a",
+	// 	text: "Переклади модів Skyrim",
+	// 	attr: "href", value: "/ut-skyrim.html"
+	// },
+	// {
+	// 	tag: "a",
+	// 	text: "Транскрибування",
+	// 	attr: "href", value: "/transcript.html"
+	// },
+	// {
+	// 	tag: "a",
+	// 	text: "Новини Hearts of Iron IV",
+	// 	attr: "href", value: "/hoi4-news"
+	// },
+	{
+		tag: "a",
+		text: "Підтримати автора",
+		attr: "href", value: "https://send.monobank.ua/jar/2hW6s9nEeY",
+		attr2: "target", value2: "_blank"
+	},
+	{
+		tag: "label",
+		text: "<input type='checkbox' onchange='switchTheme()'>✹",
+		attr: "class", value: "align noselect",
+		attr2: "title", value2: "Увімкнути світлу ѳему (налаштування не зберігається)"
+	}
 ]
 
 function generateHeader() {
 	const header = document.querySelector("header");
 	for (var i = 0; i < headerButtons.length; i++) {
-		const headerButton = document.createElement("a");
-		headerButton.setAttribute("href", headerButtons[i].link);
+		const headerButton = document.createElement(headerButtons[i].tag);
+		headerButton.setAttribute(headerButtons[i].attr, headerButtons[i].value);
+		if (headerButtons[i].attr2) { headerButton.setAttribute(headerButtons[i].attr2, headerButtons[i].value2); }
 		headerButton.innerHTML = headerButtons[i].text;
-		header.insertBefore(headerButton, header.children[i + 0]);
+		header.insertBefore(headerButton, header.children[i]);
 	}
 }
+
+function switchTheme() { document.documentElement.classList.toggle("light"); }
